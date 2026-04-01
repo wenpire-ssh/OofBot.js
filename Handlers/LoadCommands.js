68936b44fc7f9ee45e9c4890ff6b6e954db6c305
Wenpire <wesleyextremegamer14@gmail.com>
2026-04-01 12:50:51 -0300

    fix readme and loadcommands.js

--- Handlers/LoadCommands.js
+++ Handlers/LoadCommands.js
@@ -1,53 +1,15 @@
-const js = require('@eslint/js');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = [
-	js.configs.recommended,
-	{
-		languageOptions: {
-			ecmaVersion: 'latest',
-		},
-		rules: {
-			'arrow-spacing': ['warn', { before: true, after: true }],
-			'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
-			'comma-dangle': ['error', 'always-multiline'],
-			'comma-spacing': 'error',
-			'comma-style': 'error',
-			curly: ['error', 'multi-line', 'consistent'],
-			'dot-location': ['error', 'property'],
-			'handle-callback-err': 'off',
-			indent: ['error', 'tab'],
-			'keyword-spacing': 'error',
-			'max-nested-callbacks': ['error', { max: 4 }],
-			'max-statements-per-line': ['error', { max: 2 }],
-			'no-console': 'off',
-			'no-empty-function': 'error',
-			'no-floating-decimal': 'error',
-			'no-inline-comments': 'error',
-			'no-lonely-if': 'error',
-			'no-multi-spaces': 'error',
-			'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
-			'no-shadow': ['error', { allow: ['err', 'resolve', 'reject'] }],
-			'no-trailing-spaces': ['error'],
-			'no-var': 'error',
-			'no-undef': 'off',
-			'object-curly-spacing': ['error', 'always'],
-			'prefer-const': 'error',
-			quotes: ['error', 'single'],
-			semi: ['error', 'always'],
-			'space-before-blocks': 'error',
-			'space-before-function-paren': [
-				'error',
-				{
-					anonymous: 'never',
-					named: 'never',
-					asyncArrow: 'always',
-				},
-			],
-			'space-in-parens': 'error',
-			'space-infix-ops': 'error',
-			'space-unary-ops': 'error',
-			'spaced-comment': 'error',
-			yoda: 'error',
-		},
-	},
-];
+module.exports = (client) => {
+    client.commands = new Map();
+
+    const commandsPath = path.join(__dirname, "../Commands");
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for(const file of commandFiles)
+    {
+        const command = require(path.join(commandsPath, file))
+        client.commands.set(command.data.name, command);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-#OofBot.js
-##Made by Wenpire
+# OofBot.js
+## Made by Wenpire
+
+> Discord Bot made with **Discord.js 14.26**.
+
